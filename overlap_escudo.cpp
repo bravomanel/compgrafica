489fc5fff8216c43b39fdebe5affb78acb78ca08
Emanuel Bravo <emanuelbravo0706@gmail.com>
2025-04-27 11:23:54 -0300

    Corrige distorção para imagem não quadrada

--- overlap_escudo.cpp
+++ overlap_escudo.cpp
@@ -7,8 +7,10 @@ int main(){
     std::vector<vec2> CP = loadCurve("tarefa07escudo.txt");
     std::vector<vec2> P = sample_bezier_spline<3>(CP, 50);
 
-    ImageRGB I(900, 900);
+    ImageRGB I(1200, 900);
     I.fill(white);
+
+    mat3 Win = window(-4.0/3.0, 4.0/3.0, -1, 1);
 
     std::vector<vec2> positions = {
         {-1, 0.6}, {0, 0.6}, {1, 0.6}, {-1, -0.6}, {0, -0.6}, {1, -0.6}
@@ -21,7 +23,7 @@ int main(){
         for (int i = 0; i < 8; i++) {
             float s = 0.5 * pow(0.7, i);
             mat3 S = scale(s, s);
-            mat3 M = Ti * S * T;
+            mat3 M = Win * Ti * S * T;
             RGB color = RGB{uint8_t(255 - i*30), uint8_t(0 + i*30), uint8_t(0)};
             render2d(M*P, LineStrip{P.size()}, color, I);
         }
